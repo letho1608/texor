b014c7857ab583b926d7a3b618a162e9e163b71b
Le Tho <letho16082003@gmail.com>
2025-06-01 01:13:05 +0700

    Update version to 0.1.1 for PyPI release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="texor",
-    version="0.1.2",
+    version="0.1.1",
     author="letho1608",
     author_email="letho16082003@gmail.com",
     description="Lightweight native deep learning framework with PyTorch-style API",
